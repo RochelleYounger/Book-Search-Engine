4e95611877cbd81163c3864003519324ce53c4a2
Rochelle Younger <regnuoy321@gmail.com>
2022-05-03 02:14:50 -0400

    outlined typDefs and resolvers that need to implemented

--- server/schemas/resolvers/user.js
+++ server/schemas/resolvers/user.js
@@ -1,8 +1,17 @@
 // const User = require('../../models/User');
 // const Book = require('../../models/Book');
+// const { User} = require('../../models');
 
 module.exports = {
   Query: {
     hello: () => "hello world!"
-  }
-}
+    // me:
+  },
+
+  // Mutation {
+  //   // login
+  //   // addUser
+  //   // saveBook
+  //   // removeBook
+  // }
+};
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,39 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  # type User {
+  #   # _id
+  #   # username
+  #   # email
+  #   # bookCount
+  #   # saved books
+  # }
+
+  # type Book {
+  #   # bookId
+  #   # authors
+  #   # description
+  #   # title
+  #   # image
+  #   # link
+  # }
+
+  # type Auth {
+  #   # token
+  #   # user
+  # }
+
   type Query {
     hello: String
+    # me:
   }
+
+  # type Mutation {
+  # #   # login
+  # #   # addUser
+  # #   # saveBook
+  # #   # removeBook
+  # }
 `
 
 module.exports = typeDefs;
